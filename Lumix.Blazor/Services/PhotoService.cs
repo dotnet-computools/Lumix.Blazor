ff472d26a4d4c89fdfcc18bddbf0e2c1bd082b5e
Dima Sydoruk <dimonsydoruk@gmail.com>
2025-11-27 12:38:01 +0200

    Refactor ProfileCard and add avatar upload support

--- Lumix.Blazor/Services/PhotoService.cs
+++ Lumix.Blazor/Services/PhotoService.cs
@@ -28,6 +28,7 @@ public class PhotoService : IPhotoService
             using var content = new MultipartFormDataContent();
             
             content.Add(new StringContent(uploadDto.Title), "Title");
+            content.Add(new StringContent(uploadDto.IsAvatar.ToString().ToLowerInvariant()), "IsAvatar");
             
             var fileContent = new StreamContent(uploadDto.PhotoFile.OpenReadStream(maxAllowedSize: 10485760));
             fileContent.Headers.ContentType = new MediaTypeHeaderValue(uploadDto.PhotoFile.ContentType);
